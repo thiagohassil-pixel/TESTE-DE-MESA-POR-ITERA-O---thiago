4241fc7116d4f608a27279f74528ac6f6107ae60
thiagohassil-pixel <thiago.hassil@gmail.com>
2026-06-24 11:24:21 -0300

    Add files via upload

---
+++ questão 05/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        decimal valorFuturo = 7390.61m;
+        decimal taxaJuros = 0.0125m;
+        int periodo = 2 * 12;
+        decimal valorPresente = valorFuturo / (decimal)Math.Pow((double)(1 + taxaJuros), periodo);
+        Console.WriteLine($"O valor presente é: {valorPresente:C}");
+    }
+}
